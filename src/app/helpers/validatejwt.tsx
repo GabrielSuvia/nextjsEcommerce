1f519d421f8332ff5d75a1f8575be7d8008f2053
gabriel <ggabrielomar123@hotmail.com>
2024-11-13 16:21:36 -0400

    function to control the time of user

--- src/app/helpers/validatejwt.tsx
+++ src/app/helpers/validatejwt.tsx
@@ -1,16 +1,27 @@
-import Cookies from 'js-cookie';
+import { User } from "@/context/loginContext";
+import router from "next/router";
 
-export const CheckTokenExpiration = ()=>{
-const token = Cookies.get('token');
-if(!token) return;
 
-const expirationDate = Cookies.get('token-expiration')
+//handle the function with time and user
+export const CheckTokenExpiration = (timeToker:number, setUser:React.Dispatch<React.SetStateAction<User>>)=>{
+if(!timeToker) return;
+
+const expirationDate = timeToker
+console.log(expirationDate,"Time given")
+
+
 const now = new Date().getTime();
+console.log(now,"nooooowww")
 
-if(now >= Number(expirationDate)){
-    
-    logout();//exit of web
-};
-   setTimeout(CheckTokenExpiration, 60 * 1000)//verificate each minute
+if(now >= expirationDate){
+  
+console.log("seuUSerss")
+    setUser({email:"",password:""})
+    router.push('/') 
+}else{
+    console.log("contando.....")
+   setTimeout(()=>CheckTokenExpiration(timeToker,setUser), 20 * 1000)//verificate each 20seg
+}
+   
 
 }
