ceb4074f5bbb7b75687ec7980226dbd8c045dc71
gabriel <ggabrielomar123@hotmail.com>
2024-11-13 16:18:42 -0400

    receive JWT and reply with the time and token

--- src/app/api/Auth/route.tsx
+++ src/app/api/Auth/route.tsx
@@ -1,15 +1,21 @@
-import { FetchToDb } from "@/app/helpers/fetchToApi";
 import { NextRequest, NextResponse } from "next/server";
-
+//solicitado a la db bckend
 export async function POST(req:NextRequest){
-const userLogin = await req.json()
+const userLogin = await req.json()//email,password
 console.log(userLogin,"Comunicacion de la ruta")
 try {
-    console.log('login fetched',userLogin)
-    const api = 'http://localhost:3003/auth/signin'
-    await FetchToDb(api,'POST',userLogin)
- return NextResponse.json({message:"user received", user: userLogin},{status:200})
- //return NextResponse.redirect(new URL('/', req.url))
+    const api = 'http://localhost:3003/auth/signin';
+
+    const token = await fetch(api,{
+      method:'POST',
+      headers:{'Content-Type':'application/json'},
+       body:JSON.stringify(userLogin)
+   })
+   const TOKEN = await token.json();
+   if(TOKEN){
+      console.log("TOKEN: ",TOKEN)
+   }
+ return NextResponse.json({message:"user received", user: {token:TOKEN, user:userLogin}},{status:200})
 } catch (error) {
    return NextResponse.json({message:"bad request"},{status:400})
 }}
