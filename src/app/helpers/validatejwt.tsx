ef78432b254668b725d8983ae9979e823f53fd89
gabriel <ggabrielomar123@hotmail.com>
2024-11-15 09:37:22 -0400

    add parameter Router from side client component

--- src/app/helpers/validatejwt.tsx
+++ src/app/helpers/validatejwt.tsx
@@ -1,9 +1,9 @@
 import { User } from "@/context/loginContext";
-import router from "next/router";
+import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
 
 
 //handle the function with time and user
-export const CheckTokenExpiration = (timeToker:number, setUser:React.Dispatch<React.SetStateAction<User>>)=>{
+export const CheckTokenExpiration = (timeToker:number, setUser:React.Dispatch<React.SetStateAction<User>>, router:AppRouterInstance)=>{
 if(!timeToker) return;
 
 const expirationDate = timeToker
@@ -11,16 +11,14 @@ console.log(expirationDate,"Time given")
 
 
 const now = new Date().getTime();
-console.log(now,"nooooowww")
 
 if(now >= expirationDate){
   
-console.log("seuUSerss")
     setUser({email:"",password:""})
     router.push('/') 
 }else{
     console.log("contando.....")
-   setTimeout(()=>CheckTokenExpiration(timeToker,setUser), 20 * 1000)//verificate each 20seg
+   setTimeout(()=>CheckTokenExpiration(timeToker,setUser,router), 20 * 1000)//verificate each 20seg
 }
    
 
