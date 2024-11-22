afd9f8a89fab5dbdef29c477b8a59eb67a3fb4c5
gabriel <ggabrielomar123@hotmail.com>
2024-11-22 17:23:57 -0400

    button

--- src/app/components/productMarkets.tsx
+++ src/app/components/productMarkets.tsx
@@ -11,38 +11,37 @@ export interface Iproduct{
     description:string;
 }
 export interface Iarr{
-      product: ICart[]
+      data: ICart[] | [];
 }
 
-export const MarketProduct: React.FC<Iarr> = ({product})=>{
-//render each product
+export const MarketProduct: React.FC<Iarr> = ({data})=>{
+//render each product, prop === entry
 const {user,cart,setCart} = userSett()
 //objeto
-const handleBuying = (product:ICart)=>{
+const handleBuying = (products:ICart)=>{
     if(user.email === ""){
         alert('have to register you first');
          return;
     };
     console.log('SetCartAAA')
         //add al contexto
-   user.email?  setCart([...cart,product]):""
+   user.email?  setCart([...cart,products]):""
     
 }
 
-    return(<>
-    <div className="container" style={{display:'flex', flexWrap: 'wrap', gap:'100px', position:'relative',left:'200px'}}>
-    {product.map((prod,index)=>{
-        return (<div  key={index} data-testid="product-name">
-
+    return(
+    <div className="container" style={{display:'flex', flexWrap: 'wrap', gap:'100px', position:'relative',left:'200px',height:'auto'}}>
+    {data.map((prod,index)=>{
+        return (<div  style={{width:'350px', height:'230px', margin:'5px'}} key={index} data-testid="product-name">
          <h2>Product:<Link style={{textDecoration:'none'}} href={`/pages/products/${index+1}`}>{prod.name}</Link></h2>  
             <p>Price:{prod.price}</p>
             <img src="" alt="NH" />
             <p>Description:{prod.description}</p>
-            {user?<button onClick={()=>handleBuying(prod)}>Add</button>:""} 
+            <button onClick={()=>handleBuying(prod)}>Add</button>
             <br />
            </div>)
     })}
     </div>
     
-    </>)
+    )
 }
