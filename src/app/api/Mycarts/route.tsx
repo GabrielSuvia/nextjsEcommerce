bdad1c95241b8ec3e1acffcf6bdf5993b8c3c916
gabriel <ggabrielomar123@hotmail.com>
2024-11-26 11:42:00 -0400

    sending order to the backend

--- src/app/api/Mycarts/route.tsx
+++ src/app/api/Mycarts/route.tsx
@@ -1,11 +1,26 @@
+import { FetchToDb } from "@/app/helpers/fetchToApi";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST (req:NextRequest){
 //usuario y su carrito
-console.log("req of api",req)
-   const dataForSending = await req.json();
-   console.log("api",dataForSending)
+try {
+   const info = await req.json();
+  console.log("ROUTES",info)
+  const orderInfo = {userId:info.user,products:info.cart}
+   const urlServer = 'http://localhost:3003/orders/create'
+   const order = await fetch(urlServer,{
+      method:'POST',
+      headers:{'Content-Type':'application/json'},
+      body:JSON.stringify(orderInfo)
+   })
+   const dato = await order.json()
+   console.log("APIIIII",dato,":otra_",order.body, "info", orderInfo)
+   return NextResponse.json({message:"request succefully", data:dato})
 
-   return NextResponse.json({data:dataForSending})
+} catch (error) {
+   return NextResponse.json({message:"bad request"},{status:400})
+}
+   
+  
 //send data of request to db post
 }
