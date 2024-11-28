b7192e6f184bbb95fc160550a589dbc6a93bfb41
gabriel <ggabrielomar123@hotmail.com>
2024-11-28 12:02:32 -0400

    Route api of productId

--- src/app/api/search/route.tsx
+++ src/app/api/search/route.tsx
@@ -1,31 +1,30 @@
+import { Iproduct } from "@/app/components/productMarkets";
+import { FetchToDb } from "@/app/helpers/fetchToApi";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req:NextRequest){
+export async function POST(req:NextRequest){
 const {searchParams} = new URL(req.url);//key searchParams
-const query = searchParams.get('query');
-
-if(!query){
-  return NextResponse.json({message:'search query is required'}, {status:400})
-};
-
+const query = searchParams.get('query');//arreglo del id buscado o busqueda con palabras
+const prodId = await req.json();
+console.log("Route Search",prodId)
+//if(!query){
+ // return NextResponse.json({message:'search query is required'}, {status:400})
+//};
+console.log("pasando query")
 //lista obtenida de la db
-const listProducts =[{name:"celularA",price:"23",description:"dasdasd",category:"celphones"},{
-    name:"celularB",price:"23",description:"dasdasd",category:"celphones"
-},{
-    name:"celularC",price:"23",description:"dasdasd",category:"celphones"
-},{
-    name:"celularC",price:"23",description:"dasdasd",category:"celphones"
-}];
+try {
+    const url = `http://localhost:3003/Products/${prodId}`
+    const productsfound = await fetch(url)
 
-//filtramos
-const productfinded = listProducts.filter((prod)=>
-prod.name.toUpperCase()=== query.toUpperCase() ||
-prod.category.toUpperCase()=== query.toUpperCase() ||
-prod.price.toUpperCase()=== query.toUpperCase() ||
-prod.description.toUpperCase()===query.toUpperCase()
-)
-console.log("consulta: ",query.toUpperCase())
-console.log(productfinded,"Route api GEt")
-    return NextResponse.json({rest: productfinded})
-//return NextResponse.json({result:"Invalid product"})
+    if(!productsfound.ok){
+        return NextResponse.json({message:'Error Server', status:500})
+    }
+     const productList = await productsfound.json()
+     const product = productList.products
+    
+    return NextResponse.json({rest: product},{status:200})
+} catch (error) {
+    return NextResponse.json({message:"product not found"},{status:404})
+}
+   
 }
