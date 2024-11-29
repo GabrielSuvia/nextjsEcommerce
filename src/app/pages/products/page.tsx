06f2c59d552efce5788d2908019984a2cab13865
gabriel <ggabrielomar123@hotmail.com>
2024-11-29 18:07:32 -0400

    handleFilter adding fetchData and query, and deleted state setBeforeData

--- src/app/pages/products/page.tsx
+++ src/app/pages/products/page.tsx
@@ -3,50 +3,56 @@ import React, {useEffect, useState } from "react";
 import { MarketProduct } from "@/app/components/productMarkets";
 import SearchProducts  from "../../components/search";
 import { ICart } from "@/context/loginContext";
+import Router from "next/router";
 
+const Products = ()  =>{
 
-const Products = ( )  =>{
-const [dataBefore, setDataBefore] = useState<ICart[]>([]);
 const [data , setData] = useState<ICart[] | []>([])
 
-/* = [{id:'sad2-d234-342w-dsf4',name:"celular A1",  description:"dasdda",price:"34", stock:20, imgUrl:"celular",categoryid:"celular"},]
-*/
-useEffect(()=> {
-     const urlBack ='http://localhost:3003/Products' 
-     const fetchData = async () => {
-        try {
-            const res = await fetch(urlBack);
-            if(!res.ok){
-                throw new Error('Error server');
-            };
-            const response = await res.json();
-            setData(response.products)
-        } catch (error) {
-            throw new Error('There is an error')
-        }
-    };
-    fetchData();
-},[])
-
+const fetchData = async () => {
+    const urlBack ='http://localhost:3003/Products' 
+    try {
+        const res = await fetch(urlBack);
+        if(!res.ok){
+            throw new Error('Error server');
+        };
+        const response = await res.json();
+        setData(response.products)
+    } catch (error) {
+        throw new Error('There is an error')
+    }
+};
 
 const handleFilter = (category:string)=>{
- 
-    if((category === "All")){
-        console.log(dataBefore,"sinentrear")
-        if(dataBefore.length>0){
-            console.log(dataBefore,"true")
-            setData([...dataBefore])
-        }
-    }else{
-        if(data.length > 0){ 
-            setDataBefore([...data]) ;
+      fetchData();
+    if((category !== "All")){
+
+        console.log("false")
+        if(data.length > 0 && category ===""){ 
             const filterProduct = data.filter((prod)=> prod.categoryid ===category ) 
             setData([...filterProduct])
+           }else{
+           
+            if(category !== ""){
+                const query = category
+               const functionFetch = async ()=>{
+                const response = await fetch(`/api/searchPro?query=${encodeURIComponent(query.trim())}`)
+                const data = await response.json();
+                console.log("result")
+                setData([...data.rest])
+                  Router.push(`/pages/products?query=${encodeURIComponent(query.trim())}`)
+                
+                }
+                functionFetch();
            }
+       }
+    }    
+}
 
-    }
-}   //     minHeight: '100vh'
-
+useEffect(()=> {
+    fetchData();
+},[])
+  //     minHeight: '100vh'
     return (<div className="main-Conteiner" style={{background:'lightblue', minHeight: '100vh', position:'relative', margin:'0px'}}>
             
         <h1>the best products of the markets</h1>
@@ -54,7 +60,7 @@ const handleFilter = (category:string)=>{
         <h1>Category of products</h1>
 
          <ul style={{position:'relative', right:'-50px', top:'20px'}}>
-         <SearchProducts/>
+         <SearchProducts handleFilter={handleFilter}/>
          <br />
          <li>
                <button onClick={()=>handleFilter('All')} >All</button> 
@@ -72,9 +78,7 @@ const handleFilter = (category:string)=>{
             </li>
          </ul>
          <MarketProduct data ={data.length> 0?data:[]}/> 
-        
         </div>
         </div>)
 }
-
 export default Products;
