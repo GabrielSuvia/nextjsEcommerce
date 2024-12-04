2af9d1e3b9524b2f1b2aa42fc2a44afd6c4d23cb
gabriel <ggabrielomar123@hotmail.com>
2024-12-04 17:56:15 -0400

    api for searching querys

---
+++ src/app/api/searchPro/route.tsx
@@ -0,0 +1,32 @@
+import { Iproduct } from "@/app/components/productMarkets"
+import { NextRequest, NextResponse } from "next/server"
+
+export async function GET (req:NextRequest,res:NextResponse){
+
+    const {searchParams} = new URL(req.url)
+    const query = searchParams.get('query')
+try {
+    const url = 'http://localhost:3003/Products'
+    const respons = await fetch(url)
+    const productList = await respons.json()
+    const product = productList.products
+    
+    if(query && product){
+        console.log("REGEX2",query,"1111")
+        const regex = new RegExp(query,'i')
+        console.log("REGEX2","2222")    
+       
+        const findRegex = product.filter((value:Iproduct)=> regex.test(value.name) )
+        console.log("REGEX 333",findRegex)
+        return NextResponse.json({message:"request succesfully",rest: findRegex},{status:200})
+    }
+
+
+} catch (error) {
+   return NextResponse.json({message:'product not found'},{status:404}) 
+}
+
+
+}
+
+
