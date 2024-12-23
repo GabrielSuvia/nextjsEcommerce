40736c9ca63153efe073d748770e8c49e9af2dd2
gabriel <ggabrielomar123@hotmail.com>
2024-12-23 10:31:40 -0400

    deleted

--- cypress/integration/example.spec.ts
+++
@@ -1,6 +0,0 @@
-describe('E2E Test', () => {
-    it('should visit the home page', () => {
-        cy.visit('http://localhost:3000'); // Cambia la URL según sea necesario
-        cy.contains('Welcome'); // Verifica que haya un texto específico
-    });
-});
