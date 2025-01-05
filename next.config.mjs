171e0ab950ba67a20fa0f91d0c028eeb34b6d7bb
gabriel <ggabrielomar123@hotmail.com>
2025-01-05 16:55:02 -0400

    favicon fail

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,19 @@ const nextConfig = {
     eslint: {
         ignoreDuringBuilds: true, // Ignora errores de ESLint al construir
       },
+      async headers() {
+        return [
+          {
+            source: '/favicon.ico',
+            headers: [
+              {
+                key: 'Cache-Control',
+                value: 'public, max-age=31536000',
+              },
+            ],
+          },
+        ]
+      },
 };
 
 export default nextConfig;
